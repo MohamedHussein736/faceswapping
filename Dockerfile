69af8399d42c0289e6535b628b704e817ec82197
Mohamed Hussein <mohamedhussein736@gmail.com>
2023-09-06 05:14:55 +0300

    fixing the api

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+# Use an official Python runtime as a parent image
+FROM python:3.10
+
+# Set the working directory in the container
+WORKDIR .
+
+# Copy the requirements file into the container
+COPY requirements.txt .
+
+# Install any needed packages specified in requirements.txt
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Copy the rest of the application code into the container
+COPY . .
+
+# Expose the port that your FastAPI app will run on
+EXPOSE 8000
+
+# Define the command to run your FastAPI app
+CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000"]
+
+
+ 
--- main.py
+++ main.py
@@ -1,24 +1,29 @@
+# fastapi_app.py
 from fastapi import FastAPI, UploadFile, File
-from fastapi.responses import FileResponse, HTMLResponse
+from fastapi.responses import FileResponse
 import subprocess
 import os
 
-app = FastAPI()
 
-# New route for the welcome message
-@app.get("/")
-async def welcome():
-    message = """
-    <html>
-    <head>
-        <title>Welcome to FastAPI Face Swapping</title>
-    </head>
-    <body>
-        <h2>Welcome to FastAPI Face Swapping for image and video</h2>
-    </body>
-    </html>
-    """
-    return HTMLResponse(content=message)
+def format_folder(folder_path):
+    try:
+        # Confirm that the folder exists
+        if os.path.exists(folder_path):
+            # Remove all files and subfolders within the folder
+            for root, dirs, files in os.walk(folder_path, topdown=False):
+                for file in files:
+                    file_path = os.path.join(root, file)
+                    os.remove(file_path)
+                for dir_name in dirs:
+                    dir_path = os.path.join(root, dir_name)
+                    os.rmdir(dir_path)
+            print(f"Contents of folder '{folder_path}' have been formatted.")
+        else:
+            print(f"Folder '{folder_path}' does not exist.")
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+
+app = FastAPI()
 
 @app.post("/process")
 async def process_video(
@@ -27,8 +32,11 @@ async def process_video(
     output: str = "output.mp4",
     IsVideo: bool = True,
     frame_processors: str = "face_swapper",
-    similar_face_distance: float = 0.85,
+    similar_face_distance: float = 0.85,    
+    reference_face_position: int = 0
 ):
+
+    format_folder('./temp')
     # Save uploaded files temporarily
     source_path = f"temp/source{os.path.splitext(source.filename)[1]}"
     target_path = f"temp/target{os.path.splitext(target.filename)[1]}"
@@ -41,26 +49,24 @@ async def process_video(
         "python", "run.py",
         "-s", source_path,
         "-t", target_path,
-        "-o", output,
+        "-o", "temp/"+output,
         "--frame-processor", frame_processors,
+        "--reference-face-position", str(reference_face_position),
         "--similar-face-distance", str(similar_face_distance)
     ]
 
     # Remove empty strings from command
     command = [arg for arg in command if arg]
 
-    # Run the command and capture output
     try:
         subprocess.run(command, check=True)
     except subprocess.CalledProcessError as e:
         return {"error": "Processing failed"}
 
-    # Clean up temporary files
-    os.remove(source_path)
-    os.remove(target_path)
+    outputfile = "temp/" + output
     if IsVideo:
         # Return the generated video file for download
-        return FileResponse(output, media_type="video/mp4")
+        return FileResponse(outputfile, media_type="video/mp4")
     else:
         # Return the generated image file for download
-        return FileResponse(output, media_type="image/jpeg")
+        return FileResponse(outputfile, media_type="image/jpeg")
